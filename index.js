7318c75e30492fa0ed85c72c0acf0923f0e48a51
Sergio Camargo <sergio.camargo@utexas.edu>
2017-05-16 19:03:25 +0000

    Insert query info in mLab db

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+// TODO: Add async plugin to waterfall async calls
+
+var mongo = require('mongodb').MongoClient
+var dbUrl = process.env.IMAGE_SEARCH_DB_URI;
 var express = require("express")
 var app = express()
 
 var port = process.env.PORT || 8080;
 
-app.get('/', function(req,res){
-    res.send("Hello, World!");
+app.get('/api/imagesearch/:searchterm', function(req,res){
+
+    var searchTerm = req.params["searchterm"]
+    if (!searchTerm) {
+        searchTerm = ""
+    }
+    var currentTime = new Date();
+    var doc = {term : searchTerm, when : currentTime}
+    
+    // TODO: GET & DISPLAY ALL RELEVANT SEARCH RESULTS
+    
+    // Insert query info
+    mongo.connect(dbUrl, function(err, db) {
+      if(err) throw err;
+
+      var urls = db.collection('queries');
+      urls.insert(doc, function(err, data) {
+          if(err) throw err
+          db.close()
+          res.send(data.ops[0])
+      })
+    })
 })
 
 app.listen(port, function(){
-    console.log("Port listening on: " + port);
+    console.log("Port listening on: " + port)
 })
